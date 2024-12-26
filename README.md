3972652e3764e08d1a751b9fc2a6b1ca865e9433
Anders Homm <ledimestari@gmail.com>
2024-12-26 23:48:08 +0200

    inital code commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Backblaze-progress
+
+This is my script and a related grafana dashboard to post statistics about progress of the backblaze backup to a influxDB database, then to be displayed with grafana.
+
+As Backblaze personal computer backup is a software made for windows and this is a bash script it needs to be run in a WSL or some other linux environment.
+(Bb personal computer backup is also available for Macs but I have no experience with those, this might work on a Mac too but I don't know that.)
+
+elements in attached grafana json fetch data from an influxDB bucket named "Backblaze".
+if you use a bucket with a similar name, you shouldn't need to modify all the elements on the dashboard.
+
+At the end of the script is an optional extra step to save the progress into a bb_percentage.txt text file.
+This file can then be used for some other application if you want to do so.
+You can comment out or remove the part marked with comments.
+
+## setup
+
+clone the repository and install dependencies.
+
+sudo apt update
+sudo apt install curl bc grep coreutils
+chmod +x script_name.sh
+
+you can use terminal multiplexer like screen or tmux to run the script in the backgroud.
+using tmux you can do it like this:
+
+tmux new-session -d -s backblaze
+tmux send-keys -t backblaze "cd /path/to/script/folder" Enter
+tmux send-keys -t backblaze "./backblaze_to_influx.sh" Enter
---
+++ script/backblaze_to_influx.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+while true; do
+
+# Define the file paths
+# These are likely to be found at C:\ProgramData\Backblaze\bzdata 
+total_file_path="/path/to/file/bzstat_totalbackup.xml"
+remaining_file_path="/path/to/file/bzstat_remainingbackup.xml"
+bzinfo_file_path="/path/to/file/bzinfo.xml"
+
+# Define influxDB credentials
+influx_uri=""     # example http://192.168.1.131:8763/api/v2/write?org=MY_ORG&bucket=MY_BUCKET&precision=s
+influx_token=""   # example Z9m_TqBY8k2Dexamplex8vVP7CFQ_-UTqp5Krb2_7UKwy13YrjTtx29FcPRxRhoMwb3q2Udw1gQ3bHyi3gnvcy==
+
+# Get drive Guid list
+guid_list=$(cat $total_file_path | grep bzVolumeGuid | cut -d \" -f2 | sort)
+echo "$guid_list"
+
+# Process stats drive by drive
+while IFS= read -r guid; do
+    echo "Drive: $guid"
+    drive_letter=$(cat $bzinfo_file_path | grep bzVolumeGuid | grep $guid | cut -d \" -f4 | cut -d: -f1)
+    echo "  Drive letter: $drive_letter"
+    
+    # Get drive statistics
+    selected_files=$(cat $total_file_path | grep $guid | cut -d = -f3 | tr -d -c 0-9)
+    selected_b=$(cat $total_file_path | grep $guid | cut -d = -f4 | tr -d -c 0-9)
+    remaining_files=$(cat $remaining_file_path | grep $guid | cut -d = -f3 | tr -d -c 0-9)
+    remaining_b=$(cat $remaining_file_path | grep $guid | cut -d = -f4 | tr -d -c 0-9)
+
+    # Convert bytes to MB (1 MB = 1024 * 1024 bytes)
+    # Decided to do this for similarity with backblaze UI, which also talks in MB
+    selected_MB=$(echo "$selected_b / (1024 * 1024)" | bc)
+    remaining_MB=$(echo "$remaining_b / (1024 * 1024)" | bc)
+
+    echo "  Selected files:  $selected_files"
+    echo "  Selected MB:     $selected_MB"
+    echo "  Remaining files: $remaining_files"
+    echo "  Remaining MB:    $remaining_MB"
+
+    timestamp=$(date +%s)
+
+    # Post to influxdb
+    curl --request POST \
+    "$influx_uri" \
+      --header "Authorization: Token $influx_token" \
+      --header "Content-Type: text/plain; charset=utf-8" \
+      --header "Accept: application/json" \
+      --data-binary "drives,sensor_id=DRIVE_$drive_letter selected_files=$selected_files,selected_MB=$selected_MB,remaining_files=$remaining_files,remaining_MB=$remaining_MB $timestamp"
+
+    # Optional sleep between drives
+    #sleep 1
+
+done <<< "$guid_list"
+
+# Totals
+selected_files=$(cat $total_file_path | grep totnumfilesforbackup= | cut -d = -f2 | tr -d -c 0-9)
+selected_b=$(cat $total_file_path | grep totnumfilesforbackup= | cut -d = -f3 | tr -d -c 0-9)
+remaining_files=$(cat $remaining_file_path | grep remainingnumfilesforbackup= | cut -d = -f2 | tr -d -c 0-9)
+remaining_b=$(cat $remaining_file_path | grep remainingnumfilesforbackup= | cut -d = -f3 | tr -d -c 0-9)
+
+# Convert bytes to MB (1 MB = 1024 * 1024 bytes)
+selected_MB=$(echo "$selected_b / (1024 * 1024)" | bc)
+remaining_MB=$(echo "$remaining_b / (1024 * 1024)" | bc)
+
+echo "---- Totals ----"
+echo "Selected files: $selected_files"
+echo "Selected MB: $selected_MB"
+echo "Remaining files: $remaining_files"
+echo "Remaining MB: $remaining_MB"
+
+timestamp=$(date +%s)
+
+# Post to influxdb
+curl --request POST \
+"$influx_uri" \
+  --header "Authorization: Token $influx_token" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary "backblaze,sensor_id=backblaze_ui selected_files=$selected_files,selected_MB=$selected_MB,remaining_files=$remaining_files,remaining_MB=$remaining_MB $timestamp"
+
+sleep 60
+
+# An optional extra step to save the progress percentage into a text file, to be easily parsed for some other use
+output_file="bb_percentage.txt"
+
+if [ "$selected_MB" -ne 0 ]; then
+  # Calculate percentage
+  percentage=$(echo "scale=2; 100 - ($remaining_MB / $selected_MB) * 100" | bc)
+  # Save to a text file
+  echo "$percentage" > "$output_file"
+  echo "Percentage saved to $output_file"
+else
+  echo "Error: selected_MB cannot be zero." > "$output_file"
+  echo "Error: selected_MB cannot be zero."
+fi
+
+done
